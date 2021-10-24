e731c48eaee7e53cc471076ae6a8f769e4048193
Daniel J. Silva <77859738+danieljotasilva@users.noreply.github.com>
2021-10-24 10:54:55 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+const LinksSocialMedia = {
+  github: "danieljotasilva",
+  youtube: "rocketseat",
+  facebook: "dj.silva23",
+  instagram: "dnwuzhere",
+  twitter: "danieljsilva23",
+};
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute("class");
+
+    li.children[0].href = `https://${social}.com/${LinksSocialMedia[social]}`;
+  }
+}
+
+changeSocialMediaLinks();
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${LinksSocialMedia.github}`;
+
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      document.title = data.name + " - Do-While 2021"  
+      userName.textContent = data.name;
+      userBio.textContent = data.bio;
+      userLink.href = data.html_url;
+      userImage.src = data.avatar_url;
+      userLogin.textContent = data.login;
+    });
+}
+
+getGitHubProfileInfos();
